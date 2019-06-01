166e527d52f72dea8732bf9b2354d330b79bb00f
Nils Kunze <kunze@cqse.eu>
2019-06-01 23:05:57 +0200

    add new modes

--- spikeball-led.ino
+++ spikeball-led.ino
@@ -25,6 +25,8 @@ char mode = 'c';
 
 int brightness = 32;
 byte speed_div = 4;
+uint8_t gHue = 0; // rotating "base color" used by many of the patterns
+int counter = 0;
 
 WiFiServer telnetServer(23);
 WiFiClient telnetClient;
@@ -72,7 +74,7 @@ void readTelnet() {
 				speed_inp = telnetClient.read();
                 switch (speed_inp) {
                     case '0':
-                        speed_div = 0;
+                        speed_div = 1;
                         break;
                     case '1':
                         speed_div = 2;
@@ -81,7 +83,7 @@ void readTelnet() {
                         speed_div = 4;
                         break;
                     case '3':
-                        speed_div = 6;
+                        speed_div = 8;
                         break;
 
                 }
@@ -159,17 +161,101 @@ void loop() {
             break;
         case 'e':
             // moving rainbw
-            fill_rainbow(half_leds, NUM_LEDS, (ms >> speed_div) & 0xFF, 255/NUM_LEDS);
+            rainbow();
+            break;
+        case 'f':
+            // moving rainbw
+            rainbowWithGlitter();
+            break;
+        case 'g':
+            // moving rainbw
+            confetti();
+            break;
+        case 'h':
+            // moving rainbw
+            sinelon();
+            break;
+        case 'i':
+            // moving rainbw
+            bpm();
+            break;
+        case 'j':
+            // moving rainbw
+            juggle();
             break;
     }
 
     spaceOutPixels();
     FastLED.show();
     delay(10);
+
+    counter ++;
+
+    if (counter < speed_div) {
+        gHue++;
+        counter = 0;
+        }
 }
 
 void spaceOutPixels() {
     for (uint16_t i = 0; i < NUM_LEDS; i++) {
         all_leds[2*i] = half_leds[i];
     }
+}
+
+void rainbow() 
+{
+  // FastLED's built-in rainbow generator
+  fill_rainbow( half_leds, NUM_half_leds, gHue, 7);
+}
+
+void rainbowWithGlitter() 
+{
+  // built-in FastLED rainbow, plus some random sparkly glitter
+  rainbow();
+  addGlitter(80);
+}
+
+void addGlitter( fract8 chanceOfGlitter) 
+{
+  if( random8() < chanceOfGlitter) {
+    half_leds[ random16(NUM_half_leds) ] += CRGB::White;
+  }
+}
+
+void confetti() 
+{
+  // random colored speckles that blink in and fade smoothly
+  fadeToBlackBy( half_leds, NUM_half_leds, 10);
+  int pos = random16(NUM_half_leds);
+  half_leds[pos] += CHSV( gHue + random8(64), 200, 255);
+}
+
+void sinelon()
+{
+  // a colored dot sweeping back and forth, with fading trails
+  fadeToBlackBy( half_leds, NUM_half_leds, 20);
+  int pos = beatsin16( 13, 0, NUM_half_leds-1 );
+  half_leds[pos] += CHSV( gHue, 255, 192);
+}
+
+void bpm()
+{
+  // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+  uint8_t BeatsPerMinute = 62;
+  CRGBPalette16 palette = PartyColors_p;
+  uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+  for( int i = 0; i < NUM_half_leds; i++) { //9948
+    half_leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
+  }
+}
+
+void juggle() {
+  // eight colored dots, weaving in and out of sync with each other
+  fadeToBlackBy( half_leds, NUM_half_leds, 20);
+  byte dothue = 0;
+  for( int i = 0; i < 8; i++) {
+    half_leds[beatsin16( i+7, 0, NUM_half_leds-1 )] |= CHSV(dothue, 200, 255);
+    dothue += 32;
+  }
 }
