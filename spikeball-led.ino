1de793d834e01de46dfdd7e04cc0517916a40485
Nils Kunze <kunze@cqse.eu>
2019-06-01 19:33:51 +0200

    add different color options

--- spikeball-led.ino
+++ spikeball-led.ino
@@ -3,9 +3,6 @@
 #include "FastLED.h"
 #include "ESP8266WiFi.h"
 
-// How many leds are there in your strip?
-#define NUM_LEDS 178
-
 // Where is the LED data line connected?
 #define LED_DATA D3
 
@@ -15,85 +12,103 @@
 #define STASSID "Nerdsphone"
 #define STAPSK "bierbierbierbettbettbett"
 
-// Speed 0..8 (quadratic divisor of milliseconds)
-// 0 = so fast that it's definitely not recommended for photosensitive people
-// 4 = a nice pace, you could watch it all day
-// 7 = so slow, you almost don't see it moving
-// 8 and above = animation is stopped
-#define SPEED_DIV 3
-
-// state variable for brightness.
-byte bright = 1;
+// How many leds are there in your strip?
+#define ALL_LEDS 30 // 178
+#define NUM_LEDS ALL_LEDS/2
 
 // the array of leds
-CRGB leds[NUM_LEDS];
+CRGB all_leds[ALL_LEDS];
+CRGB half_leds[NUM_LEDS];
+
+// the currently selected color mode
+int mode = 0;
 
 WiFiServer telnetServer(23);
 WiFiClient telnetClient;
 
 void connectToWifi() {
-	// Init WifiManager
-	Serial.print("Connecting to ");
-	Serial.println(STASSID);
+    // Init WifiManager
+    Serial.print("Connecting to ");
+    Serial.println(STASSID);
 
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(STASSID, STAPSK);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(STASSID, STAPSK);
 
-	while(WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-	Serial.println("");
-	Serial.print("WiFi connected with IP ");
-	Serial.println(WiFi.localIP());
+    Serial.println("");
+    Serial.print("WiFi connected with IP ");
+    Serial.println(WiFi.localIP());
 }
 
 void startServer() {
-	telnetServer.begin();
-	telnetServer.setNoDelay(true);
+    telnetServer.begin();
+    telnetServer.setNoDelay(true);
 }
 
 void setup() {
-	Serial.begin(230400);
+    Serial.begin(230400);
 
-	connectToWifi();
-	startServer();
+    connectToWifi();
+    startServer();
 
-	// Initialize the LEDs
-	pinMode(LED_DATA, OUTPUT);
-	FastLED.addLeds<WS2812B, LED_DATA, GRB>(leds, NUM_LEDS);
-	FastLED.setBrightness(64);
+    // Initialize the LEDs
+    pinMode(LED_DATA, OUTPUT);
+    FastLED.addLeds<WS2812B, LED_DATA, GRB>(all_leds, ALL_LEDS);
+    FastLED.setBrightness(64);
 
-#ifdef DEBUG
-	Serial.println("\n\n === Lamp_Simple1Button.ino ===\n\n");
-#endif
+    fill_solid(all_leds, ALL_LEDS, CRGB::Black);
+    FastLED.show();
+
+    #ifdef DEBUG
+        Serial.println("\n\n === Lamp_Simple1Button.ino ===\n\n");
+    #endif
 }
 
 void loop() {
-	if(telnetServer.hasClient()) {
-		if(!telnetClient || !telnetClient.connected()) {
-			if (telnetClient) {
-				telnetClient.stop();
-			}
-			telnetClient = telnetServer.available();
-			telnetClient.flush();
-		}
-	}
+    if(telnetServer.hasClient()) {
+        if(!telnetClient || !telnetClient.connected()) {
+            if (telnetClient) {
+                telnetClient.stop();
+            }
+            telnetClient = telnetServer.available();
+            telnetClient.flush();
+        }
+    }
 
-	while(telnetClient.available()) {
-		Serial.println(telnetClient.read());
-	}
-	delay(10);
+    while(telnetClient.available()) {
+        mode = telnetClient.read();
+        Serial.println(mode);
+    }
 
-	// I usually save the current millisecond count per loop.
-	uint32_t ms = millis();
+    switch (mode) {
+        case 0:
+            // all white
+            fill_solid(half_leds, NUM_LEDS, CRGB::White);
+            // fill_solid(half_leds, 5, CRGB::Red);
+            break;
+        case 1:
+            // flashing ring
+            fill_solid(half_leds, NUM_LEDS, CRGB::Red);
+            // flashing_ring();
+            break;
+        case 2:
+            // moving rainbw
+            // fill_rainbow(half_leds, NUM_LEDS, );
+            // moving_rainbow();
+            break;
+    }
 
-	fill_solid(leds, NUM_LEDS, CRGB::Black);
-	leds[0]=CRGB::Green;
-	leds[50]=CRGB::Red;
-	leds[100]=CRGB::Red;
-	leds[150]=CRGB::Red;
-	leds[177]=CRGB::Yellow;
-	FastLED.show();
+    spaceOutPixels();
+    FastLED.show();
+    delay(10);
+}
+
+void spaceOutPixels() {
+    for (uint16_t i = 0; i < NUM_LEDS; i++) {
+        all_leds[2*i] = half_leds[i];
+    }
 }
